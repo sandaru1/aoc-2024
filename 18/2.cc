382f20d72e64d48de5ee417c7978001bac7fb408
Sandaruwan Gunathilake <sandaruwan@readyset.io>
2024-12-18 15:51:07 +0530

    Day 18

---
+++ 18/2.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <regex>
+#include <limits>
+
+using namespace std;
+
+#define pi pair<int,int>
+
+struct node {
+    int r,c,distance;
+    node(int R,int C,int dist) {
+        r = R; c = C; distance = dist;
+    }
+
+    bool operator>(const node& rhs) const
+    {
+        return distance > rhs.distance;
+    }
+};
+
+vector<string> grid;
+priority_queue<node,vector<node>,greater<node> > pq;
+
+int dr[4] = {0,1,0,-1};
+int dc[4] = {1,0,-1,0};
+
+bool visited[200][200];
+
+vector<pi> blocks;
+
+#define R 71
+#define C 71
+
+int main() {
+    while(!cin.eof()) {
+        int x,y;
+        cin >> x;
+        cin.ignore();
+        cin >> y;
+        blocks.push_back(pi(x,y));
+    }
+
+
+    for(int i=0;i<R;i++) {
+        string s = "";
+        for(int k=0;k<C;k++) s+=".";
+        grid.push_back(s);
+    }
+
+    for(int j=0;j<blocks.size();j++) {
+        grid[blocks[j].second][blocks[j].first]='#';
+
+        memset(visited,0,sizeof(visited));
+        pq.push(node(0,0,0));
+        pq.empty();
+
+        bool found = false;
+        while(pq.size()>0) {
+            node cur = pq.top();
+            pq.pop();
+            if (cur.r == R-1 && cur.c==C-1) {
+                cout << cur.distance << endl;
+                found = true;
+                break;
+            }
+            if (visited[cur.r][cur.c]) continue;
+            visited[cur.r][cur.c] = true;
+            for(int i=0;i<4;i++) {
+                int r = cur.r + dr[i];
+                int c = cur.c + dc[i];
+                if (r<0 || c<0 || r>=R || c>=C) continue;
+                if (grid[r][c]=='#') continue;
+                if (visited[r][c]) continue;
+                pq.push(node(r,c,cur.distance+1));
+            }
+        }
+
+        if (found == false) {
+            cout << blocks[j].first << "," << blocks[j].second << endl;
+            break;
+        }
+    }
+
+
+
+    return 0;
+}
